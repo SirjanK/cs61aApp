9f483ebb076b56cbf43d4404c1272df9beee0a92
devloop0 <nathreya.master@gmail.com>
2015-10-13 18:23:20 -0700

    Started anew, brand new Android Studio project.

---
+++ CS61A/app/src/androidTest/java/devloop0/cs61a/org/cs61a/ApplicationTest.java
@@ -0,0 +1,13 @@
+package devloop0.cs61a.org.cs61a;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ CS61A/app/src/main/java/devloop0/cs61a/org/cs61a/MainActivity.java
@@ -0,0 +1,13 @@
+package devloop0.cs61a.org.cs61a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
