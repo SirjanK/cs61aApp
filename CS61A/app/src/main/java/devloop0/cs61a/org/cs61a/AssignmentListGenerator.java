30d2395f7c9ae5ff0bf8a47e231183eda8612d5c
devloop0 <nathreya.master@gmail.com>
2015-10-25 18:05:18 -0700

    AssignmentListGenerator now handles date parsing.

--- CS61A/app/src/main/java/devloop0/cs61a/org/cs61a/AssignmentListGenerator.java
+++ CS61A/app/src/main/java/devloop0/cs61a/org/cs61a/AssignmentListGenerator.java
@@ -1,5 +1,9 @@
 package devloop0.cs61a.org.cs61a;
 
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 /**
@@ -16,10 +20,16 @@ public class AssignmentListGenerator {
     //  4. Assignment description
     //  5. Assignment kind (string) Lab/Homework/Project/Quiz
     public AssignmentListGenerator(ArrayList<String[]> assignmentDataList) {
-        for(String[] data : assignmentDataList) {
-            assignmentArrayList.add(new Assignment(data[0], Long.parseLong(data[1]), Long.parseLong(data[2]), data[3],
-                    (data[4] == "Lab" ? Assignment.AssignmentKind.ASSIGNMENT_KIND_LAB : (data[4] == "Homework" ? Assignment.AssignmentKind.ASSIGNMENT_KIND_HOMEWORK :
-                            (data[4] == "Quiz" ? Assignment.AssignmentKind.ASSIGNMENT_KIND_QUIZ : ( data[4] == "Project" ? Assignment.AssignmentKind.ASSIGNMENT_KIND_PROJECT : Assignment.AssignmentKind.ASSIGNMENT_KIND_NONE))))));
+        try {
+            for (String[] data : assignmentDataList) {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yy");
+                assignmentArrayList.add(new Assignment(data[0], simpleDateFormat.parse(data[1]).getTime(), simpleDateFormat.parse(data[2]).getTime(), data[3],
+                        (data[4] == "Lab" ? Assignment.AssignmentKind.ASSIGNMENT_KIND_LAB : (data[4] == "Homework" ? Assignment.AssignmentKind.ASSIGNMENT_KIND_HOMEWORK :
+                                (data[4] == "Quiz" ? Assignment.AssignmentKind.ASSIGNMENT_KIND_QUIZ : (data[4] == "Project" ? Assignment.AssignmentKind.ASSIGNMENT_KIND_PROJECT : Assignment.AssignmentKind.ASSIGNMENT_KIND_NONE))))));
+            }
+        }
+        catch(ParseException ex) {
+            Log.e("Date parsing failed", ex.getMessage());
         }
     }
 
