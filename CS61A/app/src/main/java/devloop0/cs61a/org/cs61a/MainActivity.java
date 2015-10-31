288d40c05dfafdabb58b82bcdf07bb91595ec6ca
SirjanK <sxkafle12@gmail.com>
2015-10-30 21:35:39 -0700

    Initial Parse Stuff Added

--- CS61A/app/src/main/java/devloop0/cs61a/org/cs61a/MainActivity.java
+++ CS61A/app/src/main/java/devloop0/cs61a/org/cs61a/MainActivity.java
@@ -6,11 +6,16 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Parse.initialize(this, "mEtmY7czfqvQffthH1JciErMFGD3Cmib6aNWJyzv", "j1lRCi9vcKA953eOK265TuMcvcF7lgJ3AOq90Hhu");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
         setContentView(R.layout.activity_main);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.main_assignment_list);
         recyclerView.setHasFixedSize(false);
