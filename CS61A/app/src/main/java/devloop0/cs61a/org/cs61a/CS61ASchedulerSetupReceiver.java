b8355d10fcb7785b68cf164df12a60b3e1a4a508
devloop0 <nathreya.master@gmail.com>
2015-11-06 17:54:06 -0800

    Immediate notification, then six hour gap.

--- CS61A/app/src/main/java/devloop0/cs61a/org/cs61a/CS61ASchedulerSetupReceiver.java
+++ CS61A/app/src/main/java/devloop0/cs61a/org/cs61a/CS61ASchedulerSetupReceiver.java
@@ -18,7 +18,6 @@ public class CS61ASchedulerSetupReceiver extends BroadcastReceiver {
         Intent in = new Intent(context, CS61ASchedulerEventReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, in, PendingIntent.FLAG_CANCEL_CURRENT);
         Calendar now = Calendar.getInstance();
-        now.add(Calendar.MINUTE, 1);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, now.getTimeInMillis(), 60 * 60 * 6 * 1000, pendingIntent);
     }
 }
